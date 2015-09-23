11752946b014891a1ab79a2211ff3d91f9d2a19c
Brandon Martel <brandonmartel@gmail.com>
2015-09-22 19:10:37 -0500

    Add redux logging, since its easy to inspect action stream.

--- src/store.js
+++ src/store.js
@@ -1,8 +1,11 @@
 import * as reducers from './reducers';
 import thunk from 'redux-thunk';
+import createLogger from 'redux-logger';
 import {configureUDStore, applyUDMiddleware} from './utils/redux-ud';
 
+const logger = createLogger();
+
 export function configureStore (init) {
-  const middleware = applyUDMiddleware(thunk);
+  const middleware = applyUDMiddleware(thunk, logger);
   return configureUDStore({middleware, reducers, init});
 }
